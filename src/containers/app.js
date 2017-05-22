514f2f6396a671b66ca84b21244f0e43a1bf13dd
Bill Burgess <wmhburgess@gmail.com>
2017-05-22 12:18:20 +1200

    adds basic react file structure for SQL db, wip

---
+++ src/containers/app.js
@@ -0,0 +1,14 @@
+const React = require('react')
+const { connect } = require('react-redux')
+const { Link } = require('react-router')
+
+const App = (props) => {
+  return (
+    <div>
+      hello world
+      {props.children}
+    </div>
+  )
+}
+
+module.exports = connect((state) => state)(App)
